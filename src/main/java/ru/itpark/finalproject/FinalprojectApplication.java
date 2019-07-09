a332777501009df611db2f9568a13c643aebc353
Nikrom <13nikrom13@gmail.com>
2019-07-09 16:13:41 +0300

    подготовка к добавлению на Хероку

--- src/main/java/ru/itpark/finalproject/FinalprojectApplication.java
+++ src/main/java/ru/itpark/finalproject/FinalprojectApplication.java
@@ -31,6 +31,7 @@ public class FinalprojectApplication {
                             true,
                             true,
                             true
+
                     )
             );
 
