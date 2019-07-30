45357072c2f264b35373e0f28b8dc8dea347c9ed
Nikrom <13nikrom13@gmail.com>
2019-07-30 03:36:51 +0300

    отредактировал внешний вид добавления карты

--- src/main/java/ru/itpark/finalproject/controller/UserController.java
+++ src/main/java/ru/itpark/finalproject/controller/UserController.java
@@ -1,13 +1,18 @@
 package ru.itpark.finalproject.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.itpark.finalproject.domain.User;
 
 @Controller
 public class UserController {
 
     @GetMapping("/personalArea")
-    public String personalPage(){
+    public String personalPage(Model model){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", user);
         return "user/personalArea";
     }
 }
