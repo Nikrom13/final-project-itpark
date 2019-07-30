b233f581f079d52846141f1f8b5bf64079927915
roman <roman@localhost>
2019-07-30 18:04:44 +0300

    обавил модальные окна для кнопки редактировать удалить

--- src/main/java/ru/itpark/finalproject/controller/UserController.java
+++ src/main/java/ru/itpark/finalproject/controller/UserController.java
@@ -1,18 +1,22 @@
 package ru.itpark.finalproject.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.itpark.finalproject.domain.User;
+import ru.itpark.finalproject.service.UserService;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService service;
 
     @GetMapping("/personalArea")
     public String personalPage(Model model){
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        model.addAttribute("user", user);
+        model.addAttribute("user", service.loadUserByUsername(user.getUsername()));
         return "user/personalArea";
     }
 }
